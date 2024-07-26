e22157cf5c950e0ace52540f410e5b10d1a91f56
Rene de Kat <renedekat@9lives-development.com>
2024-07-26 10:37:11 +0100

    Improved Reverb security
    By default only allow connections to websockets from current host.
    Defined in REVERB_ALLOWED_ORIGINS

--- config/reverb.php
+++ config/reverb.php
@@ -80,7 +80,7 @@ return [
                     'scheme' => env('REVERB_SCHEME', 'https'),
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
                 ],
-                'allowed_origins' => ['*'],
+                'allowed_origins' => [env('REVERB_ALLOWED_ORIGINS', 'htmx.test')],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
             ],
